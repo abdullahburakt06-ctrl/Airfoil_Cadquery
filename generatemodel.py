acae57b5cdda80d64f8de2d2e4dc0e5310b86202
abdullahburakt06-ctrl <abdullahburakt06@gmail.com>
2026-05-24 11:49:04 +0300

    Fix trailing edge issue in generatemodel.py
    now the trailing edge issue is partly fixed. There is room for improvement in how the script handles the radius of the trailing edge (the trailing edge can never be a sharp edge, the roundness is tied to no of interpolation points/resolution
    
    for sharper edges increase the resolution. In the future this will be adjustable with enterparameters.csv

--- generatemodel.py
+++ generatemodel.py
@@ -1,8 +1,8 @@
 import cadquery as cq
 import numpy as np
 from cadquery.vis import show
-from rotatepoints import rotateabout
-def airfoilList(afdata, wconstr, swept):
+
+def airfoilList(afdata, wconstr, swept, dihedral):
    
     if len(afdata) != len(wconstr):
         ValueError
@@ -11,19 +11,45 @@ def airfoilList(afdata, wconstr, swept):
         workplane_init = cq.Workplane()
         for i in range(rangevalue):
             pos = wconstr[i][0]
-            
+            c = wconstr[i][3]
+
             xvals = afdata[i][0] + swept*pos
             yvals = afdata[i][1]
+
             xytuple = list(zip(xvals, yvals))
 
             lastpos = wconstr[i][1]
             twist = wconstr[i][2]
-            print("debug" + str(i))
+            print("Iteration complete: " + str(i))
 
-            workplane_init.workplane().transformed((0, 0, twist), offset=(pos*swept, dihedral*pos, pos)).spline(xytuple, makeWire=True)
-           #15/5/2026 added twist and dihedral options. Set parameters in result=airfoilList(...)
+            #trial
+            n = len(xytuple)
+            nhalf = int(n/2)
 
-        wing = workplane_init.loft(combine=True)
+            xytuple1 = xytuple[0:nhalf]
+            xytuple2 = xytuple[nhalf::]
+
+            #determine radius necessary for radiusArc
+            xStart = xytuple1[-1][0]
+            yStart = xytuple1[-1][1]
+            xEnd = xytuple2[0][0]
+            yEnd = xytuple2[0][1]
+
+            arcRad = np.sqrt(pow(xStart - xEnd, 2) + pow(yStart-yEnd, 2))/2
+
+            (
+                workplane_init
+                .workplane()
+                .transformed((0, 0, twist), offset=(pos*swept, dihedral*pos, pos))
+                .lineTo(xvals[0], yvals[0], forConstruction=True)
+                .spline(xytuple1)
+                .radiusArc((xytuple2[0][0], xytuple2[0][1]), arcRad)
+                .spline(xytuple2)
+                .close()
+            )
+
+        wing = workplane_init.loft(combine=True, clean=True)
         show(wing)
+        return(wing)
             
 
